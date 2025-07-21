09fe32a4b251a77fb22879a54f9a982f347c3d32
pierre-canet <pierre.canet@laplateforme.io>
2025-07-21 14:54:40 +0200

    ajout du du job01

---
+++ jour01/job01/index.php
@@ -0,0 +1,16 @@
+<?php
+$str = 'La Plateforme';
+echo  $str. "<br>";
+
+$str2 = "Vive";
+$str3 = "!";
+echo $str2. " ". $str. " ". $str3. "<br>";
+$var = 6;
+echo $var. "<br>";
+$var = 10 ;
+echo $var. "<br>";
+$myBool = "true";
+echo $myBool. "<br>";
+$myBool = "false";
+echo $myBool;
+?>
