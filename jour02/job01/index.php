c24925cecb8a19fb19651bbb7f8979888ff97f84
pierre-canet <pierre.canet@laplateforme.io>
2025-07-22 13:55:08 +0200

    Ajout du dossier jour02 et du job01

---
+++ jour02/job01/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $x = 0;
+        do{
+            echo $x. "<br>";
+            $x++;
+        }while($x <42);
+        if($x = 42){
+            echo "<b><u>".$x."</u></b>". "<br>";
+            $x++;
+        }
+        do{
+             echo $x. "<br>";
+            $x++;
+        }while($x <1338)
+    ?>
+
+</body>
+</html>
