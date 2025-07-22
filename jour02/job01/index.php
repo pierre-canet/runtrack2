ff4bb0dfedfdc3b1dbc2e2615d80f8d4f168cbee
pierre-canet <pierre.canet@laplateforme.io>
2025-07-22 16:16:22 +0200

    Simplification du job01 jour02

--- jour02/job01/index.php
+++ jour02/job01/index.php
@@ -7,19 +7,14 @@
 </head>
 <body>
     <?php
-        $x = 0;
-        do{
-            echo $x. "<br>";
-            $x++;
-        }while($x <42);
-        if($x = 42){
-            echo "<b><u>".$x."</u></b>". "<br>";
-            $x++;
-        };
-        do{
-             echo $x. "<br>";
-            $x++;
-        }while($x <1338);
+        for($y =0; $y <=1337; $y++){
+            if($y == 42){
+                echo "<b><u>".$y."</u></b><br>";
+            }
+            else{
+                echo $y."<br>";
+            }
+        }
     ?>
 
 </body>
