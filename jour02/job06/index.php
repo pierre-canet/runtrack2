a1794088ee09b9c5834ab9a027f9de9df2a06af5
pierre-canet <pierre.canet@laplateforme.io>
2025-07-23 11:42:02 +0200

    Ajout du jour02 job06

---
+++ jour02/job06/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>job06</title>
+</head>
+<body>
+    <?php
+        $largeur =20;
+        $hauteur =10;
+        for($j =0; $j <$hauteur; $j++){
+            for($i =0; $i <$largeur; $i++){
+                echo "*"; 
+            }
+            echo "<br>";             
+        }     
+    ?>
+    
+</body>
+</html>
