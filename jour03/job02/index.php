7f5f9b8400e2ac43fc1a1e20410b33b2426f7b34
pierre-canet <pierre.canet@laplateforme.io>
2025-07-24 09:54:32 +0200

    Ajout du jour03 job02

---
+++ jour03/job02/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $str = "Tous ces instants seront perdus dans le temps comme les larmes sous la
+         pluie.";
+        $resultat = "";
+
+        for ($i = 0; $i < strlen($str); $i += 2) {
+            $resultat .= $str[$i];
+        }
+
+        echo $resultat;
+?>
+</body>
+</html>
